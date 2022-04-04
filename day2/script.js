df055c5dbb6dfb4e02cf398955bfc51bf47461d2
Ana Luiza dos Santos Cunha <ana.luiza@radixeng.com.br>
2022-04-04 12:49:23 -0300

    alteração com valores

--- day2/script.js
+++ day2/script.js
@@ -3,9 +3,12 @@ function  Imprimir() {
     var nome = document.getElementById("nome");
     var idade = document.getElementById("idade");
     var linguagem = document.getElementById("linguagem");
-
     var elementoRespostas = document.getElementById("respostas");
-    var respostas = "Olá " + nome + ", você tem " + idade + " anos e já está aprendendo " + linguagem + "!";
+    var respostas = "Olá " + nome.value + ", você tem " + idade.value + " anos e já está aprendendo " + linguagem.value + "!";
     elementoRespostas.innerHTML = respostas;
+
+    
 }
+
+
 
